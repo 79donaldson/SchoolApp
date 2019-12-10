279cbd4a352ceff2c8b1244b3aba5f5ad1ad2a67
Aleksey <79donaldson@gmail.com>
2019-12-10 14:39:46 +0200

    change app.js

--- app.js
+++ app.js
@@ -34,12 +34,12 @@ app.use('/api/setting', settingRouter)
 app.use('/api/subject', subjectRouter)
 app.use('/api/theme', themeRouter)
 
-if(process.env.NODE_ENV === 'production'){
+//if(process.env.NODE_ENV === 'production'){
   app.use(express.static('client/dist/client'))
 
   app.get('*', (req, res)=>{
     res.sendFile(path.resolve(__dirname, 'client', 'dist', 'client', 'index.html'))
   })
-}
+//}
 
 module.exports = app
